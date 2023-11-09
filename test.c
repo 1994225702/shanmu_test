e2a3a0e51cd17515138c006b9212c089df9769e1
shanmu <146654129+1994225702@users.noreply.github.com>
2023-11-09 23:36:35 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,221 +1,208 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-//####return value (EOF)-------fclose  / fgetc  / fputc  / fputs  / 
-               //  (NULL)------fopen   / fgets    
+
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#define LEN sizeof(struct DATA)
 //
-//int main(int argc, char* argv[])
-//{//命令行参数
-//	int a, b, sum;
-//	if (argc != 3)
-//	{
-//		printf("Error!Usage: add 1 2\n");
-//		exit(0);
-//	}
-//	a = atoi(argv[1]);
-//	b = atoi(argv[2]);
-//	sum = a + b;
-//	printf("%d + %d = %d\n", a, b,sum);
-//	return 0;
-//}
+//struct DATA
+//{
+//    long num;
+//    struct DATA* next;
+//};
 //
-////文件打开操作
-//FILE* fp;
-//fp = fopen("course.txt", "r");
-//if (fp == NULL)
+//struct DATA* create(int n)
 //{
-//	printf("Can't open the file! \n");
-//	exit(0);
-//}
-//fclose(fp);	//关闭文件----------失败返回EOF--(-1)
-
-//int main()
-//{//写入文件
-//	char ch;
-//	FILE* fp;
-//	fp = fopen("file1.txt", "w");//以w方式打开文件
-//	if (fp == NULL)
-//	{
-//		printf("打开文件失败! \n");
-//		exit(0);
-//	}
-//	while ((ch = getchar()) != '#')
-//	{
-//		fputc(ch, fp);			//把字符ch写入文件-------------fputc(char ch,FILE* fp)
-//													//		   fgetc(FILE* fp)
-//	}
-//	fclose(fp);					//关闭文件
-//	return 0;
-//}
-
-
-//int main()
-//{//复制文件
-//	FILE* fp1, * fp2;
-//	char ch, src[50], dest[50];
-//	printf("输入源文件名：");
-//	scanf("%s", src);
-//	printf("输入目标文件名：");
-//	scanf("%s", dest);
-//	if ((fp1 = fopen(src, "r")) == NULL)
-//	{
-//		printf("原文件打开失败！\n");
-//		exit(0);
-//	}
-//	if ((fp2 = fopen(dest, "w")) == NULL)
-//	{
-//		printf("目标文件打开失败！\n");
-//		exit(0);
-//	}
-//	while ((ch = fgetc(fp1)) != EOF)
-//	{
-//		fputc(ch, fp2);
-//	}
-//	fclose(fp1);
-//	fclose(fp2);
-//	printf("文件复制完成！\n");
-//	return 0;
-//}
-
-
-
-//int main()
-//{//在文件dict.txt中有n个长度不超过20的英文单词(n<10),每行一个单词，编写程序从文件中读取这些单词按升序排序后输出到output.txt文件
-//	char dict[10][21], tmp[21];
-//	int i, j, n = 0;
-//	FILE* fp;
-//	if ((fp = fopen("dict.txt", "r")) == NULL)	//读方式打开文件
-//	{
-//		printf("文件打开失败！ \n");
-//		exit(0);
-//	}
-//	while ((fgets(dict[n], 21, fp)) != NULL)	//循环从文件中读取字符串-----  char* fgets(char* buf,int n,FILE* fp)-----n为字符串长度包含结束标志
-//	{
-//		n++;									//循环结束后n为字符串个数
-//	}
-//	fclose(fp);									//关闭文件
-//	for (i = 0; i < n - 1; i++)					//冒泡排序
-//	{
-//		for (j = 0; j < n - 1 - i; j++)
-//		{
-//			if (strcmp(dict[j], dict[j + 1]) > 0)
-//			{
-//				strcpy(tmp, dict[j]);
-//				strcpy(dict[j], dict[j + 1]);
-//				strcpy(dict[j + 1], tmp);
-//			}
-//		}
-//	}
-//	if ((fp = fopen("output.txt", "w")) == NULL)	//写方式打开文件
-//	{
-//		printf("文件打开失败！\n");
-//		exit(0);
-//	}
-//	for (i = 0; i < n; i++)					//循环把字符串写入文件
-//	{
-//		fputs(dict[i], fp);					//intfputs(char*s,FILE* fp)
-//	}
-//	fclose(fp);
-//	return 0;
-//}
-
-
-//int main()
-//{//把所有4位整数写入二进制文件integer.dat中，再将所有整数从文件读出，计算所有整数的和
-//    int i, sum = 0;
-//    FILE* fp;
-//    //以二进制写入方式打开文件
-//    if ((fp = fopen("integer.dat", "wb")) == NULL)	
-//    {
-//       	printf("文件打开失败！\n");
-//        exit(0);
-//    }
-//    for (i = 1000; i <= 9999; i++)
-//    {
-//        fwrite(&i, sizeof(int), 1, fp);         //向文件中写入1个整数
-//    }                                           //int fwrite(void* p,unsigned size,unsigned n,FILE* fp)
-//    fclose(fp);
-//    //以二进制读方式打开文件
-//    if ((fp = fopen("integer.dat", "rb")) == NULL)
-//    {
-//        printf("文件打开失败！\n");
-//        exit(0);
-//    }
-//    while ((fread(&i, sizeof(int), 1, fp)) > 0)     //读取1个整数，并判断是否成功
-//    {                                               //int fread(void* p,unsigned size,unsigned n,FILE* fp)
-//        sum = sum + i;
-//    }
-//    fclose(fp);
-//    printf("sum=%d\n", sum);
-//    return 0;
-//}
-
-//typedef struct student
-//{
-//    int id;
-//    char name[20];
-//    double score;
-//}STUDENT;
-//int main()
-//{//把多个学生信息的结构体数组写入student.dat文件中
-//    STUDENT stu[] = { {1001,"Zhang",88.0},{1002,"Lee",75},{1003,"Wang",95} };
+//    struct DATA* head = NULL, * p1 = NULL, * p2 = NULL;
 //    int i;
-//    FILE* fp;
-//    if ((fp = fopen("student.dat", "wb")) == NULL)
+//    for (i = 1; i <= n; i++)
 //    {
-//        printf("文件打开失败！\n");
-//        exit(0);
+//        p1 = (struct DATA*)malloc(LEN);
+//        scanf("%ld", &p1->num);
+//        p1->next = NULL;
+//        if (i == 1) head = p1;
+//        else p2->next = p1;
+//        p2 = p1;
 //    }
-//    for (i = 0; i < 3; i++)
-//    {
-//        fwrite(&stu[i], sizeof(STUDENT), 1, fp);
-//    }//等价于==fwrite(stu,sizeof(STUDENT),3,fp);
-//    fclose(fp);
-//    return 0;
+//    return(head);
 //}
+//
+//struct DATA* merge(struct DATA* head, struct DATA* head2)
+//{
+//    /// <summary>
+//    /// 标程
+//    /// </summary>
+//    struct DATA* p;
+//    p = head;
+//    while (p->next != NULL) p = p->next;
+//    p->next = head2;
+//    return head;
 
-
-//格式化读写函数
-//int fscanf(FILE* fp,char * format,args,...)
-//int fprintf(FILE* fp,char * format,args,...)
-
-
-
-int main()
-{
-    int i = 0, sum = 0,n;
-    FILE* fp;
-    if ((fp = fopen("integer.dat", "rb")) == NULL)
+    /*if (head == NULL) return head2;
+    struct DATA* p = head; 
+    while (p->next != NULL)
     {
-        printf("文件打开失败！\n");
-        exit(0);
+        p = p->next;
     }
-    while (fread(&n, sizeof(int), 1, fp) > 0)
-    {
-        printf("i=%d,n=%d\n", i, n);
-        sum = sum + n;
-        fseek(fp, sizeof(int), SEEK_CUR);       //跳过下一个整数
-    }
-    fclose(fp);
-    printf("sum=%d\n", sum);
-    return 0;
+    p->next = head2;
+    return head;*/
 }
 
+//struct DATA* insert(struct DATA* head, struct DATA* d)
+//{
+    /*if (head == NULL) return d;
+    else if (head->num >= d->num)   
+    {
+        d->next = head;
+        return d;
+    }
+    else 
+    {
+        struct DATA * p = head,*pre;
+        while (p->next != NULL)
+        {
+            pre = p;
+            p = p->next;
+            if (p->num > d->num)
+            {
+                d->next = pre->next;
+                pre->next = d;
+                break;
+            }   
+        }
+        if (p->next == NULL)
+        {
+            d->next = p->next;
+            p->next = d;
+        }
+        
+    }*/
 
 
 
+    /*struct DATA* p0, * p1, * p2=NULL;
+    p1 = head;  p0 = d;
+    if (head == NULL)
+    {
+        head = p0;
+    }
+    else
+    {
+        while ((p0->num > p1->num) && (p1->next != NULL))
+        {
+            p2 = p1;     p1 = p1->next;
+        }
+        if (p0->num <= p1->num)
+        {
+            if (head == p1) head = p0;
+            else p2->next = p0;
+            p0->next = p1;
+        }
+        else { p1->next = p0; }
+    }
+    return head;*/
 
+    //标程
+    /*struct DATA* p0, * p1, * p2;
+    p1 = head;
+    p0 = d;
+    if (head == NULL) { head = p0; p0->next = NULL; }
+    else
+    {
+        while ((p0->num > p1->num) && (p1->next != NULL))
+        {
+            p2 = p1;
+            p1 = p1->next;
+        }
+        if (p0->num <= p1->num)
+        {
+            if (head == p1)head = p0;
+            else p2->next = p0;
+            p0->next = p1;
+        }
+        else { p1->next = p0; p0->next = NULL; }
+    }
+}
 
+struct DATA* sort(struct DATA* head)
+{
+    /// <summary>
+    /// 标程
+    /// </summary>
+    struct DATA* p1, * p2;
+    p2 = head; p1 = head;
+    p2 = p2->next;
+    p1->next = NULL;
+    p1 = p2;
+    while (p2->next != NULL)
+    {
+        p2 = p2->next;
+        p1->next = NULL;
+        head = insert(head, p1);
+        p1 = p2;
+    }
+    head = insert(head, p1);*/
 
+    /*struct DATA* p = head,*q = NULL,*head2 = NULL;
+    while (p != NULL)
+    {
+        q = p->next;
+        p->next = NULL;
+        head2 = insert(head2, p);
+        p = q;
+    }
+    head = head2;
 
+    return head;*/
+//}
 
-
-
-
-
-
+//void print(struct DATA* head)
+//{
+//    struct DATA* p;
+//    p = head;
+//    while (p != NULL)
+//    {
+//        printf("%ld", p->num);
+//        p = p->next;
+//        printf("\n");
+//    }
+//}
+//
+//main()
+//{
+//    struct DATA* head, * head2;
+//    int n;
+//    long del_num;
+//    scanf("%d", &n);
+//    head = create(n);
+//    scanf("%d", &n);
+//    head2 = create(n);
+//    head = merge(head, head2);
+//    head = sort(head);
+//    print(head);
+//}
 
 
