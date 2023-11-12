577aea1407150b6b3e4387cc01d1669b9f47bf94
shanmu <146654129+1994225702@users.noreply.github.com>
2023-11-12 23:00:55 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,208 +1,214 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-
-
-
+#pragma pack(4)//设置默认对齐数
+#include <math.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
+#include <ctype.h>
 #include <assert.h>
-#include <math.h>
+#include <stddef.h>//offsetof
+#pragma pack()//取消设置默认对齐数
 
 
-//int main()
+
+//位段-------二进制位------节省空间
+//成员必须是int /unsigned int /signed int
+
+struct S
+{
+	int a : 2;
+	int b : 10;
+	int c : 8;
+	int d : 20;
+};
+int main()
+{
+	struct S s;
+
+	printf("%d\n", sizeof(s));
+	return 0;
+}
+
+
+
+
+
+//结构体对齐规则------空间换时间--------尽量让空间小的成员集中在一起
+//1第一个成员在与结构体变量偏移量为0的地址处
+//2其他成员要对齐到某个数字对齐数的整数倍的地址处
+//3结构体总大小为最大对齐数(每个成员变量都有一个对齐数)的整数倍
+//4如果嵌套了结构体的情况，嵌套的结构体对齐到自己的最大对齐数的整数倍处，结构体的整体大小
+//就是所有最大对齐数（含嵌套结构体的对齐数）的整数倍
+//对齐数==编译器默认的一个对齐数与该成员大小的较小值
+//VS的默认值为8
+
+//建议调用结构体用传地址模式节省空间+const防止结构体被破坏
+//struct s
 //{
-//	char word[10000][11] = { 0 };
-//	FILE* fp;
-//	int i,n = 0;
-//	if (fp = fopen("case1.txt", "r") == NULL)
-//		return 0;
-//	while (fgets(word[n], 11, fp) != NULL)
-//	{
-//		n++;
-//	}
-//	fclose(fp);
-//	for(i=0;i<n;i++)
-//		for (int j = 0; j < n - i; j++)
-//		{
-//			if (strcmp(word[j], word[j + 1]) > 0)
-//			{
-//				char tmp[11];
-//				strcpy(tmp, word[j]);
-//				strcpy(word[j], word[j + 1]);
-//				strcpy(word[j + 1], tmp);
-//			}
-//		}
-//	n = 0;
-//	if (fp = fopen("answer.txt", "w") == NULL)
-//		return 0;
-//	while (fputs(word[n], fp) != EOF)
-//	{
-//		n++;
-//	}
-//	fclose(fp);
-//	return 0;
-//}
-//void move(int* arr, int sz)
-//{
-//	int right = sz-1,left = 0;
-//	while(left<right)
-//	{
-//		//从左边找偶数
-//		while (left < right && arr[left] % 2 == 1)
-//		{
-//			left++;
-//		}
-//		//从右边找奇数
-//		while (left < right && arr[right] % 2 == 0)
-//		{
-//			right--;
-//		}
-//		if (left < right)
-//		{
-//			int tmp = arr[left];
-//			arr[left] = arr[right];
-//			arr[right] = tmp;
-//		}
-//	}
-//}
-//
-//void print(int* arr, int sz)
-//{
+//	char c;
 //	int i;
-//	for (i = 0; i < sz ; i++)
-//		printf("%d ", arr[i]);
-//}
+//	double d;
+//};
+//int main()
+//{
+//	printf("%d\n", offsetof(struct s, c));//查看结构体变量偏移量
+//	printf("%d\n", offsetof(struct s, i));//宏定义
+//	printf("%d\n", offsetof(struct s, d));
+//	return 0;
 //
-//int main()
-//{
-//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-//	int sz = sizeof(arr) / sizeof(arr[0]);
-//	move(arr, sz);
-//	print(arr, sz);
-//	return 0;
-//}
-// 
-// 
-// 
-////喝汽水问题---1R1瓶汽水，两个空瓶换1瓶汽水
-// 等价交换思想，1R买一瓶，其他0.5R买一瓶
-// 或者0.5R买一瓶，手里剩一瓶没得换
-//int main()
-//{
-//	int money, total,empty;
-//	scanf("%d", &money);
-// 
-// 
-//	///2
-// 
-// if(money==0)
-// 
-// 
-// 
-// 
-// 
-//	//买回来的汽水喝掉
-//	total = money;
-//	empty = money;
-//	//换回来的汽水
-//	while (empty>=2)
-//	{
-//		total += empty / 2;
-//		empty = empty % 2 + empty / 2;
-//	}
-//	printf("total=%d", total);
-//	return 0;
 //}
 
 
 
 
-//打印水仙花数
-//int main()
-//{//各位上的数字的n次方之和等于该数，该数是n位数
-//	int i, tmp;
-//	for (i = 1; i <= 100000; i++)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//内存操作函数
+//void* memcpy(void* dest, const void* src, size_t num);
+//C语言标准
+//memcpy 只要处理 不重叠内存拷贝就可以
+//memmove 处理重叠内存的拷贝
+  
+//int memcmp(const void* ptr1,const void* ptr2,size_t num);
+
+//void* memset(void* dest,int c,size_t count);操作单位是字节byte
+//设置内容
+
+
+//void* my_memmove(void* dest, const void* src, size_t num)
+//{
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//	void* ret = dest;
+//	if (dest < src)
 //	{
-//		tmp = i;
-//		int sum = 0;
-//		int n = 1;
-//		while (tmp /= 10)
+//		//前->后
+//		while (num--)
 //		{
-//			n++;
+//			*(char*)dest = *(char*)src;
+//			++(char*)dest;
+//			++(char*)src;
 //		}
-//		tmp = i;
-//		while (tmp)
+//	}
+//	else
+//	{
+//		//后->前
+//		while (num--)
 //		{
-//			sum += (int)pow(tmp % 10, n);
-//			tmp /= 10;
+//			*((char*)dest + num) = *((char*)src + num);
 //		}
-//		if (i == sum)
-//			printf("%d ", i);
 //	}
-//	return 0;
+//	return ret;
 //}
 
 
-//int main()
-//{//a+aa+aaa+aaaa+aaaaa+...+n-a==?
-//	int i,a, n,ret =0,sum=0;
-//	scanf("%d%d", &a, &n);
-//	for (i = 0; i < n; i++)
+//void* my_memcpy(void* dest, const void* src, size_t num)
+//{
+//	void* ret = dest;
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//
+//	while (num--)
 //	{
-//		ret = ret * 10 + a;
-//		sum += ret;
+//		*(char*)dest = *(char*)src;
+//		++(char*)dest;		//优先级相同。右结合
+//		++(char*)src;
 //	}
-//	printf("%d", sum);
-//	return 0;
+//	return ret;
+//}
+//int main()
+//{
+//	int arr[] = { 12,4,5,6 };
+//	int arr1[10] = { 0 };
+//
+//	my_memcpy(arr1, arr, sizeof(arr));
+//
 //}
 
 
-////逆序函数
-//void reverse(char* str)
+
+//int main()
 //{
-//	assert(str);
-//	int len = strlen(str);
-//	char* left = str;
-//	char* right = str + len - 1;
+//	//错误码 错误信息
+//	//0 --   No error
+//	//1 --   Operation not permitted
+//	//2 --   No such file or directory
+//	//...
+//	//errno 是一个全局的错误码的变量
+//	//当c语言的库函数在执行过程中,发生了错误,就会把对应的错误码,赋值到errno中
 //
-//	while (left < right)
+//	//char* str = strerror(errno);
+//	//printf("%s\n",str);
+//
+//	//打开文件
+//	FILE* pf = fopen("test.txt", "r");
+//
+//	if (pf == NULL)
 //	{
-//		char tmp = *left;
-//		*left = *right;
-//		*right = tmp;
-//		left++;
-//		right--;
+//		printf("%s\n", strerror(errno));
 //	}
-//}
-//
-//int main()
-//{
-//	char arr[100] = { 0 };
-//	gets(arr);
-//	reverse(arr);
-//	printf("逆序后的的字符串：%s\n", arr);
+//	else
+//		printf("open file success\n");
+//	char str[] = "hello world!";
+//	fputs(str, pf);
+//	fclose(pf);
 //	return 0;
 //}
 
 
-
-
-
-
-
-
-
-
-
-
+//strtok会破坏源字符串
+//char* strtok(char* str,const char* sep);
+// 192.28.61.132
+//192 28 61 132
+//bit@163.cn
+//bit 163 cn
 //int main()
 //{
-//	char* c[] = { "ENTER","NEW","POINT","FIRST" }; //指针数组
-//	char** cp[] = { c + 3,c + 2,c + 1,c };		//二级指针数组
-//	char*** cpp = cp;							//三级指针
+//	char arr1[] = "bit@edu@.cpn@n";
+//	char* p1 = "@.";
 //
-//	printf("%s\n", **++cpp);	//POINT				//cpp自增值已经改变
-//	printf("%s\n", *--*++cpp+3);	//ER			//cpp值再次改变
-//	printf("%s\n", *cpp[-2]+3);		//ST				//cpp值不变
-//	printf("%s\n", cpp[-1][-1]+1);  //EW
-//	return 0;
+//	//点分十进制表示方式
+//	char arr[] = "192.28.61.132";
+//	char* p = ".";
+//	
+//	char buf1[1024] = { 0 };
+//	strcpy(buf1, arr1);
+//
+//	char buf[1024] = { 0 };
+//	strcpy(buf, arr);
+//	//切割buf的字符串
+//	char* ret = NULL;
+//
+//	for (ret = strtok(buf1, p1); ret != NULL; ret = strtok(NULL, p1))
+//	{
+//		printf("%s\n", ret);
+//	}
+//
+//	/*for (ret = strtok(buf, p); ret != NULL; ret = strtok(NULL, p))
+//	{
+//		printf("%s\n", ret);
+//	}*/
 //}
+
+
+
+
+
+
+
+
+
