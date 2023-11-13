7654795ebb4f792d24611e4a2c0244668236fe0d
shanmu <146654129+1994225702@users.noreply.github.com>
2023-11-13 23:53:38 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,214 +1,83 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#pragma pack(4)//设置默认对齐数
-#include <math.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-#include <ctype.h>
-#include <assert.h>
-#include <stddef.h>//offsetof
-#pragma pack()//取消设置默认对齐数
 
 
 
-//位段-------二进制位------节省空间
-//成员必须是int /unsigned int /signed int
-
-struct S
-{
-	int a : 2;
-	int b : 10;
-	int c : 8;
-	int d : 20;
-};
+//#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-	struct S s;
+    int a, n, m, x;
+    scanf("%d%d%d%d", &a, &n, &m, &x);
 
-	printf("%d\n", sizeof(s));
-	return 0;
+    int sum1[25], sum2[25];
+
+    sum1[2] = 1, sum1[3] = 2;
+
+    for (int i = 4; i < n; i++)
+    {
+        sum1[i] = sum1[i - 1] + sum1[i - 2] - 1;
+        sum2[i] = sum2[i - 1] + sum2[i - 2] + 1;
+    }
+
+    int b = (m - a * sum1[n - 1]) / sum2[n - 1];
+    printf("%d", a * sum1[x] + b * sum2[x]);
+
+    return 0;
 }
 
-
-
-
-
-//结构体对齐规则------空间换时间--------尽量让空间小的成员集中在一起
-//1第一个成员在与结构体变量偏移量为0的地址处
-//2其他成员要对齐到某个数字对齐数的整数倍的地址处
-//3结构体总大小为最大对齐数(每个成员变量都有一个对齐数)的整数倍
-//4如果嵌套了结构体的情况，嵌套的结构体对齐到自己的最大对齐数的整数倍处，结构体的整体大小
-//就是所有最大对齐数（含嵌套结构体的对齐数）的整数倍
-//对齐数==编译器默认的一个对齐数与该成员大小的较小值
-//VS的默认值为8
-
-//建议调用结构体用传地址模式节省空间+const防止结构体被破坏
-//struct s
+//联合体/共用体
+//联合体大小至少是最大成员的大小
+//当最大成员大小不是最大对齐数的整数倍的时候，就要对齐到最大对齐的整数倍
+//union Un
 //{
 //	char c;
 //	int i;
-//	double d;
+//}u1;
+//
+//
+//int main()
+//{
+//	u1.c = 'n';
+//	u1.i = 7;
+//	printf("%d\n", u1.c);
+//	printf("%d\n", u1.i);
+//	int a = 1;
+//	printf("%d", *(char*)&a);//小端字节序数据存储模式
+//	return 0;
+//}
+
+
+
+
+
+
+
+//枚举类型
+//1增加了代码的可读性和可维护性
+//2和#define定义的标识符比较枚举有类型检查，更加严谨
+//3防止了命名污染
+//4便于调试
+//5使用方便，一次可以定义多个变量
+//enum SEX
+//{
+//	//枚举的可能取值-常量
+//	MALE = 1,
+//	FEMALE =3,
+//	SECRET = 8
+//};
+//
+//enum Color
+//{
+//	RED,
+//	Green = 7,
+//	Blue
 //};
 //int main()
 //{
-//	printf("%d\n", offsetof(struct s, c));//查看结构体变量偏移量
-//	printf("%d\n", offsetof(struct s, i));//宏定义
-//	printf("%d\n", offsetof(struct s, d));
-//	return 0;
+//	enum SEX s = MALE;
 //
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//内存操作函数
-//void* memcpy(void* dest, const void* src, size_t num);
-//C语言标准
-//memcpy 只要处理 不重叠内存拷贝就可以
-//memmove 处理重叠内存的拷贝
-  
-//int memcmp(const void* ptr1,const void* ptr2,size_t num);
-
-//void* memset(void* dest,int c,size_t count);操作单位是字节byte
-//设置内容
-
-
-//void* my_memmove(void* dest, const void* src, size_t num)
-//{
-//	assert(dest != NULL);
-//	assert(src != NULL);
-//	void* ret = dest;
-//	if (dest < src)
-//	{
-//		//前->后
-//		while (num--)
-//		{
-//			*(char*)dest = *(char*)src;
-//			++(char*)dest;
-//			++(char*)src;
-//		}
-//	}
-//	else
-//	{
-//		//后->前
-//		while (num--)
-//		{
-//			*((char*)dest + num) = *((char*)src + num);
-//		}
-//	}
-//	return ret;
-//}
-
-
-//void* my_memcpy(void* dest, const void* src, size_t num)
-//{
-//	void* ret = dest;
-//	assert(dest != NULL);
-//	assert(src != NULL);
-//
-//	while (num--)
-//	{
-//		*(char*)dest = *(char*)src;
-//		++(char*)dest;		//优先级相同。右结合
-//		++(char*)src;
-//	}
-//	return ret;
-//}
-//int main()
-//{
-//	int arr[] = { 12,4,5,6 };
-//	int arr1[10] = { 0 };
-//
-//	my_memcpy(arr1, arr, sizeof(arr));
-//
-//}
-
-
-
-//int main()
-//{
-//	//错误码 错误信息
-//	//0 --   No error
-//	//1 --   Operation not permitted
-//	//2 --   No such file or directory
-//	//...
-//	//errno 是一个全局的错误码的变量
-//	//当c语言的库函数在执行过程中,发生了错误,就会把对应的错误码,赋值到errno中
-//
-//	//char* str = strerror(errno);
-//	//printf("%s\n",str);
-//
-//	//打开文件
-//	FILE* pf = fopen("test.txt", "r");
-//
-//	if (pf == NULL)
-//	{
-//		printf("%s\n", strerror(errno));
-//	}
-//	else
-//		printf("open file success\n");
-//	char str[] = "hello world!";
-//	fputs(str, pf);
-//	fclose(pf);
+//	printf("%d %d %d ", MALE, FEMALE, SECRET);
+//	printf("%d %d %d", RED, Green, Blue);
+//  printf("%d",sizeof(enum));//4---int 类型大小
 //	return 0;
 //}
-
-
-//strtok会破坏源字符串
-//char* strtok(char* str,const char* sep);
-// 192.28.61.132
-//192 28 61 132
-//bit@163.cn
-//bit 163 cn
-//int main()
-//{
-//	char arr1[] = "bit@edu@.cpn@n";
-//	char* p1 = "@.";
-//
-//	//点分十进制表示方式
-//	char arr[] = "192.28.61.132";
-//	char* p = ".";
-//	
-//	char buf1[1024] = { 0 };
-//	strcpy(buf1, arr1);
-//
-//	char buf[1024] = { 0 };
-//	strcpy(buf, arr);
-//	//切割buf的字符串
-//	char* ret = NULL;
-//
-//	for (ret = strtok(buf1, p1); ret != NULL; ret = strtok(NULL, p1))
-//	{
-//		printf("%s\n", ret);
-//	}
-//
-//	/*for (ret = strtok(buf, p); ret != NULL; ret = strtok(NULL, p))
-//	{
-//		printf("%s\n", ret);
-//	}*/
-//}
-
-
-
-
-
-
-
-
-
