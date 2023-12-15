2c8f1d6260c2414b7f603d899fe772ddc9f32532
shanmu <146654129+1994225702@users.noreply.github.com>
2023-12-15 23:31:02 +0800

    Add files via upload

---
+++ 1016.c
@@ -0,0 +1,37 @@
+/*
+1016 字符变换
+时间限制:1000MS  代码长度限制:10KB
+提交次数:4076 通过次数:2872
+
+题型: 编程题   语言: G++;GCC
+Description
+由键盘输入5个字符，将其中的大写字符变成小写（其它类型的字符不变），最后，按输入顺序输出这5个字符。
+
+
+
+输入样例
+ApPLe
+
+
+输出样例
+apple
+*/
+/*
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+    char str[6] = {0};
+    gets(str);
+
+    for(int i=0;i<5;i++)
+    {
+        printf("%c",tolower(str[i]));
+    }
+
+    return 0;
+}
+*/
