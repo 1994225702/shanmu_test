77d10aa956c20d4fda4bae654ccdb28a9d209982
shanmu <146654129+1994225702@users.noreply.github.com>
2023-12-15 23:30:43 +0800

    Add files via upload

---
+++ 1007.c
@@ -0,0 +1,42 @@
+/*
+1007 判断平方数
+时间限制:1000MS  代码长度限制:10KB
+提交次数:2309 通过次数:1543
+
+题型: 编程题   语言: G++;GCC
+Description
+由键盘输入一个正整数，判断该数是否为平方数，是输出Y，否则输出N
+
+
+
+输入格式
+一个整数
+
+
+输出格式
+Y或者N
+
+
+输入样例
+49
+
+
+输出样例
+Y
+*/
+/*
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n,tmp;
+    scanf("%d",&n);
+    tmp=(int)sqrt(n);
+    if(tmp*tmp==n)
+        printf("Y");
+    else printf("N");
+
+    return 0;
+}
+*/
