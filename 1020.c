dfaba1549f9b4b1b257927bc5acd6c0fe96b3118
shanmu <146654129+1994225702@users.noreply.github.com>
2023-12-17 23:36:25 +0800

    Add files via upload

---
+++ 1020.c
@@ -0,0 +1,39 @@
+/*
+1020 正负奇偶判断
+时间限制:1000MS  代码长度限制:10KB
+提交次数:4920 通过次数:3612
+
+题型: 编程题   语言: G++;GCC
+Description
+由键盘输入非零整数x，判断该数正负，正数输出positive，负数输出negative，接着判断该数的奇偶性，奇数输出odd，偶数输出even。
+
+
+
+输出格式
+注意，正负判断结果与奇偶判断结果之间用回车符分隔
+
+
+输入样例
+-43
+
+
+输出样例
+negative
+odd
+*/
+/*
+#include <stdio.h>
+
+
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x>0) printf("positive\n");
+    else printf("negative\n");
+    if(x%2==0) printf("even\n");
+    else printf("odd\n");
+
+    return  0;
+}
+*/
