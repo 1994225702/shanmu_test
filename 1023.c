855a0664fee5c4fce6efb8075576b39ed90c3886
shanmu <146654129+1994225702@users.noreply.github.com>
2023-12-18 23:27:16 +0800

    Add files via upload

---
+++ 1023.c
@@ -0,0 +1,49 @@
+/*
+1023 简单计算器
+时间限制:1000MS  代码长度限制:10KB
+提交次数:4226 通过次数:2975
+
+题型: 编程题   语言: G++;GCC
+Description 下面程序是实现一个简单的运算器（保留两位小数点），如果由键盘输入10+50，计算机可以输出结果60.00；如果输入8＊6，计算机输出48.00；如果输入20/4，计算机输出5.00；如果输入8-6，计算机输出2.00，请在空处填上适当的代码，运行通过后并提交。
+
+
+
+
+
+输入样例
+45*2
+
+
+输出样例
+result=90.00
+*/
+/*
+#include <stdio.h>
+
+int main()
+{
+    float a,b,c;
+    char op;
+    scanf("%f%c%f",&a,&op,&b);
+    switch(op)
+    {
+    case '+':
+        c=a+b;
+        break;
+    case '-':
+        c=a-b;
+        break;
+    case '*':
+        c=a*b;
+        break;
+    case '/':
+        c=a/b;
+        break;
+    default:
+        printf("error");
+        return 0;
+    }
+    printf("result=%.2f",c);
+    return 0;
+}
+*/
