7c93ca35026d7df2be56ebba92d5b6b5a4d7e79f
shanmu <146654129+1994225702@users.noreply.github.com>
2023-12-19 23:02:58 +0800

    Add files via upload

---
+++ 1024.c
@@ -0,0 +1,29 @@
+/*
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double x,y;
+    scanf("%lf,%lf",&x,&y);
+
+    if(fabs((x*x+y*y)-1)<1e-3)
+        printf("Y");
+    else printf("N");
+
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+
+int main()
+{
+    int n,sum=1,i;
+    scanf("%d",&n);
+
+    for(i=2;i<=n;i++)
+        sum*=i;
+    printf("%d",sum);
+    return 0;
+}
+*/
