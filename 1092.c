e40fc55bbaaf413f1db7a47268f49d1c8878570c
shanmu <146654129+1994225702@users.noreply.github.com>
2023-12-29 19:52:36 +0800

    Add files via upload

---
+++ 1092.c
@@ -0,0 +1,42 @@
+/*
+1092 [填空]函数实现求字符串长度
+时间限制:1000MS  代码长度限制:10KB
+提交次数:2715 通过次数:1733
+
+题型: 填空题   语言: GCC
+Description
+下面程序实现由函数实现求字符串长度，再填空完成
+
+
+
+输入样例
+Hello!
+
+
+输出样例
+6
+*/
+
+
+
+//#include "stdio.h"
+//
+///*create function f*/
+//int f(char* s)
+//{
+//    char* cur = s;
+//    while(*cur!='\0')
+//    {
+//        cur++;
+//    }
+//    return cur-s;
+//}
+//
+//int main()
+//{
+//    char s[80];
+//    int i;
+//    scanf("%s", s);
+//    i=f(s);
+//    printf("%d", i);
+//}
