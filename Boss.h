c14e07ed8a4ff9d71932e9ee7a386a6bc9e0ea13
shanmu <146654129+1994225702@users.noreply.github.com>
2024-02-01 18:53:47 +0800

    Add files via upload

---
+++ Boss.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Worker.h"
+
+class Boss :public Worker
+{
+public:
+	Boss(int id, string name, int dId);
+
+	virtual void showInfo();
+
+	virtual string getDeptName();
+
+
+};
