6ba01d8beebff7dc474904b52db7a6b38eb9d416
shanmu <146654129+1994225702@users.noreply.github.com>
2024-02-23 20:59:53 +0800

    Add files via upload

---
+++ Worker.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+#include <string>
+
+class Worker
+{
+public:
+	virtual void showInfo() = 0;
+
+	virtual string getDeptName() = 0;
+
+	int m_Id;
+	string m_Name;
+	int m_DeptId;
+};
